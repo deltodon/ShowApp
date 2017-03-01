90cbd5ee8358208395bd4be2a2d9d9cbf7107ccc
deltodon <web@deltodon.com>
2017-03-01 12:51:31 +0000

    Add demo style

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 var mainWindow = null;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({
+        title: "ShowApp " + app.getVersion(),
+        backgroundColor: "#333333",
+        width: 1000,
+        height: 800
+    })
 
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 })
