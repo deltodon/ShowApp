2925787ded156180ebb2b3b7aee4de424c8464f0
deltodon <web@deltodon.com>
2017-04-14 11:49:11 +0100

    Clean up settings section

--- src/render-process/users.js
+++
@@ -1,8 +0,0 @@
-var studentName = document.getElementById('student-name');
-var projectName = document.getElementById('proj-name');
-var projectDescribtion = document.getElementById('proj-describtion');
-
-
-studentName.innerHTML = "Alex";
-projectName.innerHTML = "Hello World!";
-projectDescribtion.innerHTML = "learning to walk.";
